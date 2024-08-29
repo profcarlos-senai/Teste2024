ee24138936ce6224f3c1188cbe46dbe81eb5ea24
Carlos Correia <carlos.correia@sistemafiep.org.br>
2024-08-29 18:51:49 -0300

    Criado Teste03

---
+++ Decimal1/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double x = 0.1;
+        double y = 0.2;
+        double z = 0.3;
+        System.out.printf("%.20f\n", x);
+        System.out.printf("%.20f\n", y);
+        System.out.printf("%.20f\n", z);
+    }
+}
