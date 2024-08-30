cea18b8fa9d147db8dbc9e537b81dba27544aaab
emanuelicalza <emanuelimanu06@gmail.com>
2024-08-29 21:47:27 -0300

    Fiz os testes

--- Teste03/src/main/java/CalcIMC.java
+++ Teste03/src/main/java/CalcIMC.java
@@ -1,2 +1,54 @@
 public class CalcIMC {
+    private double altura;
+    private double peso;
+
+    public CalcIMC(double altura, double peso) {
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public double getIMC(){
+        return (peso / (altura*altura));
+    }
+
+    public String getDescrIMC(){
+        double imc = getIMC();
+        if (imc < 19) {
+            return "magrelo";
+        } else
+        if (imc < 25) {
+            return "normal";
+        } else {
+            return "thais carla";
+        }
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+
+    public double getMin() {
+        return (19*(altura*altura));
+    }
+
+    public double getMax() {
+        return (25*(altura*altura));
+    }
+
+    public double getAlturaSaudavel() {
+        return Math.sqrt(peso/22);
+    }
 }
--- Teste03/src/test/java/CalcIMCTest.java
+++ Teste03/src/test/java/CalcIMCTest.java
@@ -1,2 +1,55 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CalcIMCTest {
+
+    private CalcIMC calc;
+
+
+    @Test
+    void testaCriacaoObjeto(){
+        calc = new CalcIMC(1.70,60);
+        assertNotNull(calc);
+
+        assertEquals(1.70, calc.getAltura());
+        assertEquals(60, calc.getPeso());
+    }
+    @Test
+    void testaCalcIMC(){
+        calc = new CalcIMC(1.70,60);
+        assertEquals(20.76, calc.getIMC(),0.01);
+    }
+
+    @Test
+    void testaDescriMagro(){
+        calc = new CalcIMC(1.70,10);
+        assertEquals("magrelo",calc.getDescrIMC());
+    }
+
+    @Test
+    void testaDescriNormal(){
+        calc = new CalcIMC(1.70,60);
+        assertEquals("normal",calc.getDescrIMC());
+    }
+
+    @Test
+    void testaDescriThais(){
+        calc = new CalcIMC(1.70,180);
+        assertEquals("thais carla",calc.getDescrIMC());
+    }
+
+    @Test
+    void testaMinMax(){
+        calc = new CalcIMC(1.70,60);
+        assertEquals(54.91, calc.getMin(),0.01);
+
+        assertEquals(72.25, calc.getMax(),0.01);
+    }
+
+    @Test
+    void testaAlturaSaldavel(){
+        calc = new CalcIMC(1.70,60);
+        assertEquals(1.65, calc.getAlturaSaudavel(),0.01);
+    }
 }
