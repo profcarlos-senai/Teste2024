4c5eba22435fb37bcdb218d64f59fc5171ace5ba
MateusBeiraMar <thegame.nando.fmmk@gmail.com>
2024-08-29 21:37:42 -0300

    aaaaaa

--- Teste03/src/main/java/CalcIMC.java
+++ Teste03/src/main/java/CalcIMC.java
@@ -1,2 +1,81 @@
 public class CalcIMC {
+    private double peso;
+    private double altura;
+    private double imc;
+    private String descrIMC;
+    private double min;
+    private double max;
+    private double alRecomendavel;
+
+    public CalcIMC(double peso, double altura) {
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public void calculaIMC(){
+        setImc(getPeso()/(getAltura()*getAltura()));
+        if (getImc() <= 19){
+            setDescrIMC("magrelo");
+        } else if (getImc() <= 25) {
+            setDescrIMC("normal");
+        }else{
+            setDescrIMC("gordo");
+        }
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getImc() {
+        return imc;
+    }
+
+    public void setImc(double imc) {
+        this.imc = imc;
+    }
+
+    public String getDescrIMC() {
+        return descrIMC;
+    }
+
+    public void setDescrIMC(String descrIMC) {
+        this.descrIMC = descrIMC;
+    }
+
+    public double getMin() {
+        return 19*(getAltura()*getAltura());
+    }
+
+    public void setMin(double min) {
+        this.min = min;
+    }
+
+    public double getMax() {
+        return 25*(getAltura()*getAltura());
+    }
+
+    public void setMax(double max) {
+        this.max = max;
+    }
+
+    public double getAlRecomendavel() {
+        return Math.sqrt(getPeso()/getImc());
+    }
+
+    public void setAlRecomendavel(double alRecomendavel) {
+        this.alRecomendavel = alRecomendavel;
+    }
 }
--- Teste03/src/test/java/CalcIMCTest.java
+++ Teste03/src/test/java/CalcIMCTest.java
@@ -1,2 +1,58 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CalcIMCTest {
+    private CalcIMC calcImc;
+
+    @Test
+    void testCriacaoObjeto(){
+        calcImc = new CalcIMC(1.0,2.0);
+        assertNotNull(calcImc);
+        assertEquals(1, calcImc.getPeso());
+        assertEquals(2, calcImc.getAltura());
+    }
+
+    @Test
+    void testgetIMC(){
+        calcImc = new CalcIMC(1.0,2.0);
+        calcImc.calculaIMC();
+        assertEquals(0.25, calcImc.getImc());
+    }
+
+    @Test
+    void testgetMin(){
+        calcImc = new CalcIMC(1.0,2.0);
+        assertEquals(76, calcImc.getMin());
+    }
+
+    @Test
+    void testgetMax(){
+        calcImc = new CalcIMC(1.0,2.0);
+        assertEquals(100, calcImc.getMax());
+    }
+
+    @Test
+    public void testCalculoIMC() {
+        calcImc = new CalcIMC(60.0, 1.8);
+        calcImc.calculaIMC();
+        assertEquals("magrelo", calcImc.getDescrIMC());
+
+        calcImc = new CalcIMC(80.0, 1.8);
+        calcImc.calculaIMC();
+        assertEquals("normal", calcImc.getDescrIMC());
+
+        calcImc = new CalcIMC(100, 1);
+        calcImc.calculaIMC();
+        assertEquals("gordo", calcImc.getDescrIMC());
+    }
+
+    @Test
+    void testAlRecomendavel(){
+        calcImc = new CalcIMC(60.0, 1.8);
+        calcImc.calculaIMC();
+        double alRecomendavel = Math.sqrt(calcImc.getPeso() / calcImc.getImc());
+        assertEquals(alRecomendavel, calcImc.getAlRecomendavel());
+    }
+
 }
